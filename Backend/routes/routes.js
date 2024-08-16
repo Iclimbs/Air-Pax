e6380cc772491ffcf479aff32bef7e7ace547177
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-08-16 18:23:30 +0530

    Added NextJS Template For Admin Dashboard

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const { busRouter } = require("../controller/bus");
+const { tripRouter } = require("../controller/trip");
 const { userRouter } = require("../controller/user");
 
 router
     .use("/user", userRouter)
     .use("/bus",busRouter)
+    .use("trip",tripRouter)
 
 module.exports = router;
