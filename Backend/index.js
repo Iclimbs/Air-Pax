0251cc04039de7d5745119503fba9be9851bd841
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-08-29 17:22:44 +0530

    Removed AWS SMS Services

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,10 @@
 require("dotenv").config();
-const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns');
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 const connection = require("./connection/connection");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/v1/", require("./routes/routes"));
 
