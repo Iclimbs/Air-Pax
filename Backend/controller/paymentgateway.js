ab975efb2fe3fd25297396a721ee062205632c42
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-10-03 17:34:50 +0530

    Adding Auto Route

--- Backend/controller/paymentgateway.js
+++ Backend/controller/paymentgateway.js
@@ -28,12 +28,13 @@ PaymentGateway.post("/payment-status", async (req, res) => {
     });
 
     req.on('end', function () {
-		var pData = '';
+		let pData = '';
 		pData = '<table border=1 cellspacing=2 cellpadding=2><tr><td>'
 		pData = pData + ccavResponse.replace(/=/gi, '</td><td>')
 		pData = pData.replace(/&/gi, '</td></tr><tr><td>')
 		pData = pData + '</td></tr></table>'
-		htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' + pData + '</center><br></body></html>';
+        console.log("P data",pData);
+		htmlcode = `<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' + pData + '</center><br></body><script>function redirectToPage() {window.location.href=https://airpax.co/payment/${pData.order_id}}window.onload = redirectToPage;</script></html>`;
 		res.writeHeader(200, { "Content-Type": "text/html" });
 		res.write(htmlcode);
 		res.end();
