3f163c42994506913bbe3a05a2b17856d713086b
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-10-03 17:17:19 +0530

    Fixed Undefined Issue

--- Backend/controller/paymentgateway.js
+++ Backend/controller/paymentgateway.js
@@ -18,13 +18,12 @@ PaymentGateway.post("/payment-status", async (req, res) => {
 
     //Initializing Vector and then convert in base64 string
     let ivBase64 = Buffer.from([0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f]).toString('base64');
-    // let ccavEncResponse = encResp;
-    // let ccavPOST = qs.parse(ccavEncResponse);
-    // let encryption = ccavPOST.encResp
+    let ccavEncResponse = "";
+    let ccavPOST = "";
     req.on('data', function (data) {
         ccavEncResponse += data;
         ccavPOST = qs.parse(ccavEncResponse);
-        var encryption = ccavPOST.encResp;
+        let encryption = ccavPOST.encResp;
         ccavResponse = ccav.decrypt(encryption, keyBase64, ivBase64);
     });
 
