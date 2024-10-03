c2afa3f050d8331d452c957a3b4485efc9e7fe18
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-10-03 16:48:49 +0530

    Testing

--- Backend/controller/paymentgateway.js
+++ Backend/controller/paymentgateway.js
@@ -34,9 +34,9 @@ PaymentGateway.post("/payment-status", async (req, res) => {
 		pData = pData.replace(/&/gi, '</td></tr><tr><td>')
 		pData = pData + '</td></tr></table>'
 		htmlcode = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Response Handler</title></head><body><center><font size="4" color="blue"><b>Response Page</b></font><br>' + pData + '</center><br></body></html>';
-		response.writeHeader(200, { "Content-Type": "text/html" });
-		response.write(htmlcode);
-		response.end();
+		res.writeHeader(200, { "Content-Type": "text/html" });
+		res.write(htmlcode);
+		res.end();
 	});
 
 })
