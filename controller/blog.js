425e08eb117fe09e5a6d67cc3aaf95605691cd0e
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-11-13 18:37:54 +0530

    Making Changes in BLog ROutes

--- controller/blog.js
+++ controller/blog.js
@@ -31,11 +31,11 @@ BlogRouter.post("/popularblog/add", upload.single("img"), async (req, res) => {
         await newpopularblog.save()
         res.json({ status: "success", message: "New Popular Blog Added !!" })
     } catch (error) {
-        res.json({ status: "error", message: `Failed To Add New Blog ${error.message}` })
+        res.json({ status: "error", message: `Failed To Add New Popular Blog ${error.message}` })
     }
 })
 
-BlogRouter.patch("/popularblog/edit/:id",upload.single("img"), async (req, res) => {
+BlogRouter.patch("/popularblog/edit/:id", upload.single("img"), async (req, res) => {
     const { id } = req.params
     try {
         const popularblog = await PopularBlogModel.findByIdAndUpdate({ _id: id }, req.body)
@@ -52,27 +52,35 @@ BlogRouter.patch("/popularblog/disable/:id", async (req, res) => {
         const popularblog = await PopularBlogModel.findById({ _id: id })
         popularblog.status = !popularblog.status;
         await popularblog.save()
-        res.json({ status: "success", message: "Food Item Availability Updated !!" })
+        res.json({ status: "success", message: "Popular Blog Item Availability Updated !!" })
     } catch (error) {
-        res.json({ status: "error", message: "Failed To Update Food Item Availability Details" })
+        res.json({ status: "error", message: `Failed To Update Popular Blog Item Availability ${error.message}` })
     }
 })
 
 BlogRouter.get("/popularblog/listall", async (req, res) => {
     try {
         const popularBlogList = await PopularBlogModel.find({})
-        res.json({ status: "success", data: popularBlogList })
+        if (popularBlogList.length !== 0) {
+            res.json({ status: "success", data: popularBlogList })
+        } else {
+            res.json({ status: "error", message: "No Blog Found In Popular Seaction" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Popular Blog List ${error.message}` })
     }
 })
 
 BlogRouter.get("/popularblog/listall/active", async (req, res) => {
     try {
         const popularBlogList = await PopularBlogModel.find({ status: true })
-        res.json({ status: "success", data: popularBlogList })
+        if (popularBlogList.length !== 0) {
+            res.json({ status: "success", data: popularBlogList })
+        } else {
+            res.json({ status: "error", message: "No Active Blog Found In Popular Seaction" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Active Popular Blog List ${error.message}` })
     }
 })
 
@@ -91,7 +99,7 @@ BlogRouter.post("/activitycardblog/add", upload.single("img"), async (req, res) 
     }
 })
 
-BlogRouter.patch("/activitycardblog/edit/:id", async (req, res) => {
+BlogRouter.patch("/activitycardblog/edit/:id", upload.single("img"), async (req, res) => {
     const { id } = req.params
     try {
         const activitycardblog = await ActivityCardBlogModel.findByIdAndUpdate({ _id: id }, req.body)
@@ -107,7 +115,7 @@ BlogRouter.patch("/activitycardblog/disable/:id", async (req, res) => {
     const { id } = req.params
     try {
         const activityblog = await ActivityCardBlogModel.findById({ _id: id })
-        activityblog.available = !activityblog.available;
+        activityblog.status = !activityblog.status;
         await activityblog.save()
         res.json({ status: "success", message: "Activity Card Availability Updated !!" })
     } catch (error) {
@@ -118,19 +126,27 @@ BlogRouter.patch("/activitycardblog/disable/:id", async (req, res) => {
 
 BlogRouter.get("/activitycardblog/listall", async (req, res) => {
     try {
-        const foodList = await ActivityCardBlogModel.find()
-        res.json({ status: "success", data: foodList })
+        const activityCardList = await ActivityCardBlogModel.find()
+        if (activityCardList.length !== 0) {
+            res.json({ status: "success", data: activityCardList })
+        } else {
+            res.json({ status: "error", message: "No Blog Found In Activity Seaction" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Activity Blog List ${error.message}` })
     }
 })
 
 BlogRouter.get("/activitycardblog/listall/active", async (req, res) => {
     try {
         const activityCardList = await ActivityCardBlogModel.find({ status: true })
-        res.json({ status: "success", data: activityCardList })
+        if (activityCardList.length !== 0) {
+            res.json({ status: "success", data: activityCardList })
+        } else {
+            res.json({ status: "error", message: "No Active Blog Found In Activity Seaction" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Activity Blog List ${error.message}` })
     }
 })
 
@@ -148,12 +164,12 @@ BlogRouter.post("/activity/add", upload.single("img"), async (req, res) => {
     }
 })
 
-BlogRouter.patch("/activity/edit/:id", async (req, res) => {
+BlogRouter.patch("/activity/edit/:id", upload.single("img"), async (req, res) => {
     const { id } = req.params
     try {
         const activityblog = await ActivityBlogModel.findByIdAndUpdate({ _id: id }, req.body)
         await activityblog.save()
-        res.json({ status: "success", message: "Food Item Details Successfully Updated !!" })
+        res.json({ status: "success", message: "Activity Details Successfully Updated !!" })
     } catch (error) {
         res.json({ status: "error", message: `Failed To Update Activity Blog Details ${error.message}` })
     }
@@ -166,28 +182,36 @@ BlogRouter.patch("/activity/disable/:id", async (req, res) => {
         const activityblog = await ActivityBlogModel.findById({ _id: id })
         activityblog.status = !activityblog.status;
         await activityblog.save()
-        res.json({ status: "success", message: "Food Item Availability Updated !!" })
+        res.json({ status: "success", message: "Activity Details Availability Updated !!" })
     } catch (error) {
-        res.json({ status: "error", message: "Failed To Update Food Item Availability Details" })
+        res.json({ status: "error", message: `Failed To Update Food Item Availability Details ${error.message}` })
     }
 })
 
 
 BlogRouter.get("/activity/listall", async (req, res) => {
     try {
-        const foodList = await FoodModel.find()
-        res.json({ status: "success", data: foodList })
+        const activityList = await ActivityBlogModel.find()
+        if (activityList.length > 0) {
+            res.json({ status: "success", data: activityList })
+        } else {
+            res.json({ status: "error", message: "No Blog Found in Activity Section" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Activity List ${error.message}` })
     }
 })
 
 BlogRouter.get("/activity/listall/active", async (req, res) => {
     try {
-        const activityCardList = await ActivityCardBlogModel.find({ status: true })
-        res.json({ status: "success", data: activityCardList })
+        const activityList = await ActivityBlogModel.find({ status: true })
+        if (activityList.length > 0) {
+            res.json({ status: "success", data: activityList })
+        } else {
+            res.json({ status: "error", message: "No Active Blog Found in Activity Section" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Active Activity List ${error.message}` })
     }
 })
 
@@ -206,7 +230,7 @@ BlogRouter.post("/featuredblog/add", upload.single("img"), async (req, res) => {
     }
 })
 
-BlogRouter.patch("/featuredblog/edit/:id", async (req, res) => {
+BlogRouter.patch("/featuredblog/edit/:id", upload.single("img"),async (req, res) => {
     const { id } = req.params
     try {
         const featuredList = await FeaturedBlogModel.findByIdAndUpdate({ _id: id }, req.body)
@@ -234,18 +258,26 @@ BlogRouter.patch("/featuredblog/disable/:id", async (req, res) => {
 BlogRouter.get("/featuredblog/listall", async (req, res) => {
     try {
         const featuredList = await FeaturedBlogModel.find()
-        res.json({ status: "success", data: featuredList })
+        if (featuredList.length > 0) {
+            res.json({ status: "success", data: featuredList })
+        } else {
+            res.json({ status: "error", message: "No Featured Blog Found in Featured Section" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Featured List Item Details ${error.message}` })
     }
 })
 
 BlogRouter.get("/featuredblog/listall/active", async (req, res) => {
     try {
         const featuredList = await FeaturedBlogModel.find({ status: true })
-        res.json({ status: "success", data: featuredList })
+        if (featuredList.length > 0) {
+            res.json({ status: "success", data: featuredList })
+        } else {
+            res.json({ status: "error", message: "No Active Featured Blog Found in Featured Section" })
+        }
     } catch (error) {
-        res.json({ status: "error", message: error.message })
+        res.json({ status: "error", message: `Failed To Get Active Featured List Item Details ${error.message}` })
     }
 })
 
