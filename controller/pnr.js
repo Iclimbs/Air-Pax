83110004a9149d27f0d2e75df16ffb8e2bfb2652
uttamiclimbs <uttamkrshaw@iclimbs.com>
2024-11-28 13:28:50 +0530

    uploading New Changes to PNR

--- controller/pnr.js
+++ controller/pnr.js
@@ -3,6 +3,7 @@ const { TripModel } = require("../model/trip.model");
 const { OtherUserModel } = require('../model/Other.seat.model');
 const { SeatModel } = require("../model/seat.model");
 const { VehicleModel } = require("../model/vehicle.model");
+const { BookingModel } = require("../model/booking.model");
 const PnrRouter = express.Router()
 
 PnrRouter.get("/", async (req, res) => {
@@ -51,6 +52,11 @@ PnrRouter.get("/guest", async (req, res) => {
     // Getting the list of Seats Booked 
     const ticketdetails = await SeatModel.find({ pnr: pnr, "details.email": email }, { _id: 0, CreatedAt: 0 })
 
+    const bookingdetails = await BookingModel.find({pnr:pnr,status:"Confirmed"})
+    if (bookingdetails.length==0) {
+        return res.json({ status: "error", message: "No Confirmed Booking Detail's Found Related to this Pnr" })
+    }
+
     // Sending No Ticket Detail's Found Message To User
     if (ticketdetails.length == 0) {
         return res.json({ status: "error", message: "No Ticket Detail's Found Related to this Pnr" })
@@ -67,6 +73,7 @@ PnrRouter.get("/guest", async (req, res) => {
     if (tripdetails.length == 0) {
         return res.json({ status: "error", message: "No Trip Detail's Found Related to this Pnr" })
     }
+    details.bookingId=bookingdetails[0]._id
     details.tripId = tripdetails[0]._id;
     details.from = tripdetails[0].from;
     details.to = tripdetails[0].to;
--- controller/ticket.js
+++ controller/ticket.js
@@ -1,20 +1,18 @@
 const express = require("express")
+const ejs = require("ejs")
+const path = require('node:path');
+const jwt = require('jsonwebtoken');
+const { transporter } = require('../service/transporter');
 const { TripModel } = require("../model/trip.model");
 const { OtherUserModel } = require('../model/Other.seat.model');
 const { PaymentModel } = require("../model/payment.model");
-const ejs = require("ejs")
-const path = require('node:path');
 const { SeatModel } = require("../model/seat.model");
 const TicketRouter = express.Router()
-const jwt = require('jsonwebtoken');
 const { BookingModel } = require("../model/booking.model");
 const { UserAuthentication } = require("../middleware/Authentication");
 const { UserModel } = require("../model/user.model");
-const { transporter } = require('../service/transporter');
 const { OtpModel } = require("../model/otp.model");
 const otpGenerator = require('otp-generator')
-
-
 
 TicketRouter.post("/gmr/cancel", async (req, res) => {
     const { tripId, bookingRefId, pnr, cancelticket } = req.body;
@@ -328,10 +326,12 @@ TicketRouter.post("/cancel", UserAuthentication, async (req, res) => {
 TicketRouter.post("/otp/generate", async (req, res) => {
     const { pnr, tripId, seatNumbers } = req.body
 
-    let alreadyCreatedOtp = false;
-    let allconfirmed = true;
-    let seats = []
+    // let alreadyCreatedOtp = false;
+    // let allconfirmed = true;
+    let seats = [];
+    let emails = [];
     const otpExists = await OtpModel.find({ pnr: pnr, tripId: tripId })
+    const randomOtp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false })
 
 
     if (otpExists) {
@@ -341,12 +341,28 @@ TicketRouter.post("/otp/generate", async (req, res) => {
     }
 
     const hasCommonValues = seatNumbers.some(value => seats.includes(value));
+
     if (hasCommonValues) {
         res.json({ status: "error", message: `Otp Already Generated For Following Seat which Assigned To Following PNR. Please Try After Some Time !!` })
     }
+
+
+    const bookedSeats = await SeatModel.find({ pnr: pnr, tripId: tripId, seatNumber: { $in: seatNumbers }, "details.status": "Confirmed" })
+
+    if (bookedSeats.length === 0) {
+        res.json({ status: "error", message: `NO Confirmed Seat Found Which Is Assigned To Following PNR OR TRIPID !!` })
+    }
+
+
+    for (let index = 0; index < bookedSeats.length; index++) {
+        if (emails.includes(bookedSeats[index].details.email) === false) {
+            emails.push(bookedSeats[index].details.email)
+        }
+    }
+
     try {
         const newOtp = new OtpModel({
-            pnr, tripId, seatNumbers, otp: otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false }),
+            pnr, tripId, seatNumbers, otp: randomOtp,
             expireAt: Date.now() + 5 * 60 * 1000, // Lock for 5 minutes
         })
         await newOtp.save()
@@ -354,28 +370,100 @@ TicketRouter.post("/otp/generate", async (req, res) => {
         res.json({ status: "error", message: `Unable To Generate New OTP For Ticket Management of Guest ${error.message}` })
     }
 
-    // let cancelTicket = path.join(__dirname, "../emailtemplate/cancelTicket.ejs")
-    // ejs.renderFile(cancelTicket, { user: userdetails[0], seat: cancelledSeats, trip: tripdetails[0], amount: paymentdetails[0].refundamount, pnr: pnr, reason: reasonForCancellation }, function (err, template) {
-    //     if (err) {
-    //         res.json({ status: "error", message: err.message })
-    //     } else {
-    //         const mailOptions = {
-    //             from: process.env.emailuser,
-    //             to: `${userdetails[0].email}`,
-    //             subject: `Booking Cancellation on AIRPAX, Bus: ${tripdetails[0].busid}, ${tripdetails[0].journeystartdate}, ${tripdetails[0].from} - ${tripdetails[0].to}`,
-    //             html: template
-    //         }
-    //         transporter.sendMail(mailOptions, (error, info) => {
-    //             if (error) {
-    //                 console.log("Error in Sending Mail ", error.message);
-    //                 return res.json({ status: "error", message: 'Failed to send email' });
-    //             } else {
-    //                 console.log("Email Sent ", info);
-    //                 return res.json({ status: "success", message: 'Please Check Your Email', redirect: "/" });
-    //             }
-    //         })
-    //     }
-    // })
+    let guestmanagebookingoptverification = path.join(__dirname, "../emailtemplate/guestmanagebookingoptverification.ejs")
+    ejs.renderFile(guestmanagebookingoptverification, { otp: randomOtp }, function (err, template) {
+        if (err) {
+            res.json({ status: "error", message: err.message })
+        } else {
+            const mailOptions = {
+                from: process.env.emailuser,
+                to: `${emails}`,
+                subject: `OTP Verification For, Booking Cancellation on AIRPAX`,
+                html: template
+            }
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log("Error in Sending Mail ", error.message);
+                    return res.json({ status: "error", message: 'Failed to send email' });
+                } else {
+                    console.log("Email Sent ", info);
+                    return res.json({ status: "success", message: 'Please Check Your Email & Verifiy OTP Within 5 Minutes', redirect: "/" });
+                }
+            })
+        }
+    })
 })
+
+TicketRouter.post("/cancel/guest", async (req, res) => {
+    const { pnr, tripId, seatNumbers } = req.body
+
+    // let alreadyCreatedOtp = false;
+    // let allconfirmed = true;
+    let seats = [];
+    let emails = [];
+    const otpExists = await OtpModel.find({ pnr: pnr, tripId: tripId })
+    const randomOtp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false, lowerCaseAlphabets: false })
+
+
+    if (otpExists) {
+        for (let index = 0; index < otpExists.length; index++) {
+            seats.push(...otpExists[index].seatNumbers)
+        }
+    }
+
+    const hasCommonValues = seatNumbers.some(value => seats.includes(value));
+
+    if (hasCommonValues) {
+        res.json({ status: "error", message: `Otp Already Generated For Following Seat which Assigned To Following PNR. Please Try After Some Time !!` })
+    }
+
+
+    const bookedSeats = await SeatModel.find({ pnr: pnr, tripId: tripId, seatNumber: { $in: seatNumbers }, "details.status": "Confirmed" })
+
+    if (bookedSeats.length === 0) {
+        res.json({ status: "error", message: `NO Confirmed Seat Found Which Is Assigned To Following PNR OR TRIPID !!` })
+    }
+
+
+    for (let index = 0; index < bookedSeats.length; index++) {
+        if (emails.includes(bookedSeats[index].details.email) === false) {
+            emails.push(bookedSeats[index].details.email)
+        }
+    }
+    
+    try {
+        const newOtp = new OtpModel({
+            pnr, tripId, seatNumbers, otp: randomOtp,
+            expireAt: Date.now() + 5 * 60 * 1000, // Lock for 5 minutes
+        })
+        await newOtp.save()
+    } catch (error) {
+        res.json({ status: "error", message: `Unable To Generate New OTP For Ticket Management of Guest ${error.message}` })
+    }
+
+    let guestmanagebookingoptverification = path.join(__dirname, "../emailtemplate/guestmanagebookingoptverification.ejs")
+    ejs.renderFile(guestmanagebookingoptverification, { otp: randomOtp }, function (err, template) {
+        if (err) {
+            res.json({ status: "error", message: err.message })
+        } else {
+            const mailOptions = {
+                from: process.env.emailuser,
+                to: `${emails}`,
+                subject: `OTP Verification For, Booking Cancellation on AIRPAX`,
+                html: template
+            }
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log("Error in Sending Mail ", error.message);
+                    return res.json({ status: "error", message: 'Failed to send email' });
+                } else {
+                    console.log("Email Sent ", info);
+                    return res.json({ status: "success", message: 'Please Check Your Email & Verifiy OTP Within 5 Minutes', redirect: "/" });
+                }
+            })
+        }
+    })
+})
+
 
 module.exports = { TicketRouter } 
